832d591ea1ec2efe5b66402fdcdcc50535a2dc75
Pedro Henrique de Barros Olegario <pedro.olegario@sga.pucminas.br>
2025-10-15 19:31:47 -0300

    Delete src directory

--- src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-export function Logo({ className = '' }: { className?: string }) {
-  return (
-    <div className={`flex items-center justify-center ${className}`}>
-      <div className="text-4xl font-bold text-slate-700">
-        <span className="text-slate-600">ARTEMIS</span>
-      </div>
-    </div>
-  );
-}
